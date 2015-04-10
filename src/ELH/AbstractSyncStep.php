7c24ad5557463bdb46a98d04fc7b98bd94ffdcde
Luca Tumedei <luca@theaveragedev.com>
2015-04-10 08:03:42 +0200

    code format

--- src/ELH/AbstractSyncStep.php
+++ src/ELH/AbstractSyncStep.php
@@ -1,45 +1,44 @@
 <?php
 
-abstract class ELH_AbstractSyncStep implements ELH_ApIUserInterface, ELH_KeychainUserInterface, ELH_StepInterface
-{
 
-    /**
-     * @var ELH_KeychainInterface
-     */
-    protected $keychain;
-    /**
-     * @var ELH_ApiInterface
-     */
-    protected $api;
+	abstract class ELH_AbstractSyncStep implements ELH_ApIUserInterface, ELH_KeychainUserInterface, ELH_StepInterface {
 
-    /**
-     * @var ELH_StatusInterface
-     */
-    protected $status;
+		/**
+		 * @var ELH_KeychainInterface
+		 */
+		protected $keychain;
 
-    /**
-     * @var ELH_StepInterface
-     */
-    private $next;
+		/**
+		 * @var ELH_ApiInterface
+		 */
+		protected $api;
 
-    public function set_api(ELH_ApiInterface $api)
-    {
-        $this->api = $api;
-    }
+		/**
+		 * @var ELH_StatusInterface
+		 */
+		protected $status;
 
-    public function set_keychain(ELH_KeychainInterface $keychain)
-    {
-        $this->keychain = $keychain;
-    }
+		/**
+		 * @var ELH_StepInterface
+		 */
+		private $next;
 
-    public function set_next(ELH_StepInterface $next)
-    {
-        $this->next = $next;
-    }
+		public function set_api( ELH_ApiInterface $api ) {
+			$this->api = $api;
+		}
 
-    abstract public function run();
+		public function set_keychain( ELH_KeychainInterface $keychain ) {
+			$this->keychain = $keychain;
+		}
 
-   public function set_status(ELH_StatusInterface $status){
-       $this->status = $status;
-   }
-}
+		public function set_next( ELH_StepInterface $next ) {
+			$this->next = $next;
+		}
+
+		public function set_status( ELH_StatusInterface $status ) {
+			$this->status = $status;
+		}
+
+		abstract public function run();
+
+	}
