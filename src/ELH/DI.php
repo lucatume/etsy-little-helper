f3d026d22c6af23d8530ea2f990fd27830501b80
Luca Tumedei <luca@theaveragedev.com>
2015-04-10 09:35:24 +0200

    modified di wrapper to expose instance chainable methods

--- src/ELH/DI.php
+++ src/ELH/DI.php
@@ -8,6 +8,9 @@
 		 */
 		protected static $container;
 
+		/**
+		 * @return tad_DI52_Container
+		 */
 		public static function instance() {
 			if ( empty( self::$container ) ) {
 				self::$container = new tad_DI52_Container();
@@ -16,27 +19,26 @@
 			return self::$container;
 		}
 
-		public static function set_dependencies() {
-			$container = self::instance();
+		/**
+		 * @return ELH_DI
+		 */
+		public function set_dependencies() {
+			$this->set_ctor( 'keychain', 'ELH_Keychain' );
+			$this->set_ctor( 'api', 'ELH_Api' );
+			$this->set_ctor( 'sync_strategy_selector', 'ELH_SyncStrategySelector' );
 
-			$container->set_var( 'sync_retry_interval', 3600 );
-			$container->set_var( 'sync_retry_attempts', 3 );
+			// Sync steps
+			$dependencies = array( '@keychain', '@api' );
+			$this->set_ctor( 'listings_retriever', 'ELH_ListingsRetriever::instance', $dependencies );
+			$this->set_ctor( 'shop_retriever', 'ELH_ShopRetriever::instance', $dependencies )
+			     ->set_next( '@listings_retriever' );
 
-			$container->set_ctor( 'keychain', 'ELH_Keychain' );
-			$container->set_ctor( 'api', 'ELH_Api' );
+			// Sync set up
+			$dependencies = array( '@sync_strategy_selector' );
+			$this->set_ctor( 'synchronizer', 'ELH_Synchronizer::instance', $dependencies )
+			     ->set_first_step( '@shop_retriever' );
 
-			$args = array( '@keychain', '@api' );
-			$container->set_ctor( 'shop_retriever', 'ELH_ShopRetriever::instance', $args );
-
-			$args = array( '@keychain', '@api' );
-			$container->set_ctor( 'listings_retriever', 'ELH_ListingsRetriever::instance', $args );
-
-			$container->set_ctor( 'synchronizer', 'ELH_Synchronizer::instance', $args )
-			          ->add_step( '@shop_retriever' )
-			          ->add_step( '@listings_retriever' )
-			          ->set_retry_interval( '#sync_retry_interval' )
-			          ->set_retry_attempts( '#sync_retry_attempts' )
-			          ->set_hook( ELH_Main::SYNC_HOOK );
+			return $this;
 		}
 
 	}
