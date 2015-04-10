40c2acbdc128d8dc3da87cc207ba1216b299b23c
Luca Tumedei <luca@theaveragedev.com>
2015-04-10 08:45:04 +0200

    restored broken by sync files

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,15 +1,7 @@
 <?php
 	// This is global bootstrap for autoloading
-<<<<<<< HEAD
 	use tad\FunctionMocker\FunctionMocker as Test;
 
 	require dirname( dirname( __FILE__ ) ) . '/vendor/autoload.php';
 
 	Test::init();
-=======
-	use tad\FunctionMocker\FunctionMocker;
-
-	require dirname( dirname( __FILE__ ) ) . '/vendor/autoload.php';
-
-	FunctionMocker::init();
->>>>>>> 825db8a79a04feb6f8c31fe4b2ef6b4f0657835d
--- tests/unit/SynchronizerTest.php
+++ tests/unit/SynchronizerTest.php
@@ -10,27 +10,11 @@
 		 */
 		protected $sut;
 
-<<<<<<< HEAD
-=======
-		protected function setUp() {
-			Test::setUp();
-			$this->sut = new ELH_Synchronizer();
-			Test::replace( 'get_option', 0 );
-			Test::replace( 'wp_schedule_single_event' );
-			Test::replace( 'update_option' );
-		}
-
-		protected function tearDown() {
-			Test::tearDown();
-		}
-
->>>>>>> 825db8a79a04feb6f8c31fe4b2ef6b4f0657835d
 		/**
 		 * @test
 		 * it should run the first step
 		 */
 		public function it_should_run_the_first_step() {
-<<<<<<< HEAD
 			$step = Test::replace( 'ELH_StepInterface::run' );
 			$sut  = new ELH_Synchronizer();
 			$sut->set_first_step( $step );
@@ -38,19 +22,10 @@
 			$sut->sync();
 
 			$step->wasCalledOnce( 'run' );
-=======
-			$step = Test::replace( 'ELH_SyncStepInterface::sync_or_throw' );
-			$this->sut->add_step( $step );
-
-			$this->sut->sync();
-
-			$step->wasCalledOnce( 'sync_or_throw' );
->>>>>>> 825db8a79a04feb6f8c31fe4b2ef6b4f0657835d
 		}
 
 		/**
 		 * @test
-<<<<<<< HEAD
 		 * it should set the status on the first step
 		 */
 		public function it_should_set_the_status_on_the_first_step() {
@@ -61,26 +36,10 @@
 			$sut->sync();
 
 			$step->wasCalledWithOnce( [ Test::isInstanceOf( 'ELH_Status' ) ], 'set_status' );
-=======
-		 * it should catch exceptions thrown by the steps
-		 */
-		public function it_should_catch_exceptions_thrown_by_the_steps() {
-			$step = Test::replace( 'ELH_SyncStepInterface::sync_or_throw', function () {
-				throw new ELH_SyncException( 'Some error' );
-			} );
-
-			$this->sut->add_step( $step );
-
-			$status = $this->sut->sync();
-
-			Test::assertFalse( $status->did_sync );
-			Test::assertEquals( 'Some error', $status->message );
->>>>>>> 825db8a79a04feb6f8c31fe4b2ef6b4f0657835d
 		}
 
 		/**
 		 * @test
-<<<<<<< HEAD
 		 * it should capture sync exceptions and pass them to the strategy handler
 		 */
 		public function it_should_capture_sync_exceptions_and_pass_them_to_the_strategy_handler() {
@@ -101,27 +60,10 @@
 				Test::isInstanceOf( 'ELH_SyncException' ),
 				Test::isInstanceOf( 'ELH_StatusInterface' ),
 			], 'get_strategy_for' );
-=======
-		 * it should reschedule a single event to attempt another sync
-		 */
-		public function it_should_reschedule_a_single_event_to_attempt_another_sync() {
-			$step = Test::replace( 'ELH_SyncStepInterface::sync_or_throw', function () {
-				throw new ELH_SyncException( 'Some error' );
-			} );
-
-			$wp_schedule_single_event = Test::replace( 'wp_schedule_single_event' );
-
-			$this->sut->add_step( $step );
-
-			$status = $this->sut->sync();
-
-			$wp_schedule_single_event->wasCalledOnce();
->>>>>>> 825db8a79a04feb6f8c31fe4b2ef6b4f0657835d
 		}
 
 		/**
 		 * @test
-<<<<<<< HEAD
 		 * it should let the strategy selector handle the exception
 		 */
 		public function it_should_let_the_strategy_selector_handle_the_exception() {
@@ -150,21 +92,6 @@
 
 		protected function tearDown() {
 			Test::tearDown();
-=======
-		 * it should not reschedule on failure if over attempts
-		 */
-		public function it_should_not_reschedule_on_failure_if_over_attempts() {
-			$step = Test::replace( 'ELH_SyncStepInterface::sync_or_throw', function () {
-				throw new ELH_SyncException( 'Some error' );
-			} );
-			Test::replace( 'get_option', 12 );
-
-			$wp_schedule_single_event = Test::replace( 'wp_schedule_single_event' );
-
-			$this->sut->add_step( $step );
-
-			$wp_schedule_single_event->wasNotCalled();
->>>>>>> 825db8a79a04feb6f8c31fe4b2ef6b4f0657835d
 		}
 
 	}
