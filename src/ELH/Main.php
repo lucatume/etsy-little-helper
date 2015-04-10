818d79b4c1d3b97f334f758fe09b2ea1ed985e2f
Luca Tumedei <luca@theaveragedev.com>
2015-04-10 09:35:53 +0200

    modified Main to allow injection and use of di_container

--- src/ELH/Main.php
+++ src/ELH/Main.php
@@ -5,15 +5,23 @@
 
 		const SYNC_HOOK = 'elh_daily_sync';
 
-		public static function instance() {
-			return new self;
+		/**
+		 * @var ELH_DI
+		 */
+		protected $di_container;
+
+		public static function instance( ELH_DI $di_container ) {
+			$instance = new self;
+
+			$instance->set_di_container( $di_container );
+
+			return $instance;
 		}
 
 		public function hook() {
 			register_activation_hook( ELH_PLUGIN_FILE, array( $this, 'activate' ) );
 			register_deactivation_hook( ELH_PLUGIN_FILE, array( $this, 'deactivate' ) );
 			add_action( 'plugins_loaded', array( $this, 'load_text_domain' ) );
-			wp_schedule_event( time(), 'hourly', self::SYNC_HOOK );
 
 			$synchronizer = ELH_DI::instance()->make( 'synchronizer' );
 
@@ -21,7 +29,7 @@
 		}
 
 		public function activate() {
-
+			wp_schedule_event( time(), 'hourly', self::SYNC_HOOK );
 		}
 
 		public function deactivate() {
@@ -30,5 +38,9 @@
 
 		public function load_text_domain() {
 			load_plugin_textdomain( 'elh', false, dirname( plugin_basename( ELH_PLUGIN_FILE ) ) . '/languages' );
+		}
+
+		private function set_di_container( ELH_DI $di_container ) {
+			$this->di_container = $di_container;
 		}
 	}
