92f7d04b69bd2ca701d1ec57a657973ebfc2bbbb
Luca Tumedei <luca@theaveragedev.com>
2015-04-10 09:37:08 +0200

    added missing concrete sync strategy selector

---
+++ src/ELH/SyncStrategySelector.php
@@ -0,0 +1,14 @@
+<?php
+
+class ELH_SyncStrategySelector implements ELH_StrategySelectorInterface{
+
+	/**
+	 * @param Exception  $e
+	 * @param mixed|null $data
+	 *
+	 * @return ELH_StrategyInterface
+	 */
+	public function get_strategy_for( Exception $e, $data = null ) {
+		// TODO: Implement get_strategy_for() method.
+	}
+}
