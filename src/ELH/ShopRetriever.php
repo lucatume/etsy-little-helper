4e19162ecc28a3c74dade09c2392ced9f53636b3
Luca Tumedei <luca@theaveragedev.com>
2015-04-10 07:57:57 +0200

    removed shop retriever instance method

--- src/ELH/ShopRetriever.php
+++ src/ELH/ShopRetriever.php
@@ -3,14 +3,6 @@
 
 	class ELH_ShopRetriever extends ELH_AbstractSyncStep {
 
-		public static function instance( ELH_KeychainInterface $keychain, ELH_ApiInterface $api ) {
-			$instance           = new self();
-			$instance->keychain = $keychain;
-			$instance->api      = $api;
-
-			return $instance;
-		}
-
 		public function run() {
 			// TODO: Implement run() method.
 		}
