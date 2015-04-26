2d768d681c79cfde4143a92f64c1740ce3c58fce
Luca Tumedei <luca@theaveragedev.com>
2015-04-26 21:09:00 +0200

    refactored the class code to expose more granular methods

--- src/ELH/TypeUtils.php
+++ src/ELH/TypeUtils.php
@@ -24,65 +24,25 @@ class ELH_TypeUtils {
 			foreach ( $types as $_type ) {
 				switch ( $_type ) {
 					case 'string':
-						$check = $check || is_string( $value );
+						$check = $check || self::is_string( $value );
 						break;
 					case 'int':
-						$check = $check || ( is_numeric( $value ) && intval( $value ) == floatval( $value ) );
+						$check = $check || self::is_int( $value );
 						break;
 					case 'float':
-						$check = $check || is_numeric( $value ) && ( is_float( floatval( $value ) ) || is_int( intval( $value ) ) );
+						$check = $check || self::is_float( $value );
 						break;
 					case 'bool':
 						$check = true;
 						break;
 					case 'hsv';
-						$m = array();
-						if ( preg_match( '/^(\\d+);(\\d+);(\\d+)$/', $value, $m ) ) {
-							$check = $check || $m[1] >= '0' && $m[2] >= '0' && $m[3] >= '0' && $m[1] <= '360' && $m[2] <= '100' && $m[3] <= '100';
-						}
+						$check = $check || self::is_hsv( $value );
 						break;
 					case 'rgb';
-						$check = $check || preg_match( '/^#*(?:[0-9a-fA-F]{3}){1,2}$/', $value );
+						$check = $check || self::is_rgb( $value );
 						break;
 					default:
-//	new ELH_ApiRequestParameter('sort_on', false, 'created', '{created,price,score}'),
-//	new ELH_ApiRequestParameter('sort_order', false, 'down', '{up,down}'),
-//	new ELH_ApiRequestParameter('min_price', false, false, 'float'),
-//	new ELH_ApiRequestParameter('max_price', false, false, 'float'),
-//	new ELH_ApiRequestParameter('color', false, false, 'color'),
-//	new ELH_ApiRequestParameter('color_accuracy', false, 0, '[0-30]'),
-//	new ELH_ApiRequestParameter('tags', false, false, '[string]'),
-//	new ELH_ApiRequestParameter('category', false, false, 'string'),
-//	new ELH_ApiRequestParameter('translate_keywords', false, 0, 'bool'),
-//	new ELH_ApiRequestParameter('include_private', false, 0, 'bool'),
-						$m = array();
-						// enum
-						if ( preg_match( '/^\\{(.+)\\}$/', $_type, $m ) ) {
-							$legit = $m[1];
-							$check = $check || in_array( $value, explode( ',', $legit ) );
-							break;
-						}
-						if ( preg_match( '/^\\[(.+)-(.+)\\]$/', $_type, $m ) ) {
-							$check = $check || in_array( $value, range( $m[1], $m[2] ) );
-							break;
-						}
-						if ( preg_match( '/^\\[(\w+)\\]$/', $_type, $m ) ) {
-							if ( is_string( $value ) ) {
-								$check = true;
-								if ( ! strpos( $value, ',' ) ) {
-									break;
-								}
-								foreach ( explode( ',', $value ) as $v ) {
-									if ( ! self::is_a( $v, $m[1] ) ) {
-										$check = false;
-										break;
-									}
-								}
-
-							}
-							break;
-						}
-						$check = $check || false;
+						$check = $check || self::is_in_enum( $value, $_type ) || self::is_in_interval( $value, $_type ) || self::is_in_list( $value, $_type );
 						break;
 				}
 			}
@@ -90,5 +50,72 @@ class ELH_TypeUtils {
 		}
 
 		return $check;
+	}
+
+	public static function is_string( $value ) {
+		return is_string( $value );
+	}
+
+	public static function is_int( $value ) {
+		return ( is_numeric( $value ) && intval( $value ) == floatval( $value ) );
+	}
+
+	public static function is_float( $value ) {
+		return is_numeric( $value ) && ( is_float( floatval( $value ) ) || is_int( intval( $value ) ) );
+	}
+
+	public static function is_hsv( $value ) {
+		$m = array();
+		if ( preg_match( '/^(\\d+);(\\d+);(\\d+)$/', $value, $m ) ) {
+			return $m[1] >= '0' && $m[2] >= '0' && $m[3] >= '0' && $m[1] <= '360' && $m[2] <= '100' && $m[3] <= '100';
+		}
+
+		return false;
+	}
+
+	public static function is_rgb( $value ) {
+		return preg_match( '/^#*(?:[0-9a-fA-F]{3}){1,2}$/', $value );
+	}
+
+	public static function is_in_enum( $value, $type ) {
+		$m = array();
+		if ( preg_match( '/^\\{(.+)\\}$/', $type, $m ) ) {
+			$legit = $m[1];
+
+			return in_array( $value, explode( ',', $legit ) );
+		}
+
+		return false;
+	}
+
+	public static function is_in_interval( $value, $_type ) {
+		$m = array();
+		if ( preg_match( '/^\\[(.+)-(.+)\\]$/', $_type, $m ) ) {
+			return in_array( $value, range( $m[1], $m[2] ) );
+		}
+
+		return false;
+	}
+
+	public static function is_in_list( $value, $_type ) {
+		$m = array();
+		if ( preg_match( '/^\\[(\w+)\\]$/', $_type, $m ) ) {
+			if ( is_string( $value ) ) {
+				if ( ! strpos( $value, ',' ) ) {
+					return true;
+				}
+				foreach ( explode( ',', $value ) as $v ) {
+					if ( ! self::is_a( $v, $m[1] ) ) {
+						return false;
+					}
+
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		return false;
 	}
 }
