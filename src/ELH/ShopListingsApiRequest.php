cc1db509888643f8b352c57968825949da8464f4
Luca Tumedei <luca@theaveragedev.com>
2015-04-26 20:52:50 +0200

    modified the listings request to match the documentation one

--- src/ELH/ShopListingsApiRequest.php
+++ src/ELH/ShopListingsApiRequest.php
@@ -1,59 +1,63 @@
 <?php
 
-class ELH_ShopListingsApiRequest implements ELH_ApiRequestInterface
-{
 
-    private $parameters;
+class ELH_ShopListingsApiRequest implements ELH_ApiRequestInterface {
 
-    public function __construct()
-    {
-        $this->parameters = array(
-            new ELH_ApiRequestParameter('user_id', true, false, 'string|int'),
-            new ELH_ApiRequestParameter('limit', false, 25, 'int'),
-            new ELH_ApiRequestParameter('offset', false, false, 'int'),
-            new ELH_ApiRequestParameter('page', false, false, 'int'),
-        );
-    }
+	private $parameters;
 
-    /**
-     * @return string
-     */
-    public function uri()
-    {
-        return '/users/:user_id/shops';
-    }
+	public function __construct() {
+		$this->parameters = array(
+			new ELH_ApiRequestParameter( 'shop_id', true, false, 'string|int' ),
+			new ELH_ApiRequestParameter( 'limit', false, 25, 'int' ),
+			new ELH_ApiRequestParameter( 'offset', false, 0, 'int' ),
+			new ELH_ApiRequestParameter( 'page', false, false, 'int' ),
+			new ELH_ApiRequestParameter( 'keywords', false, false, 'string' ),
+			new ELH_ApiRequestParameter( 'sort_on', false, 'created', '{created,price,score}' ),
+			new ELH_ApiRequestParameter( 'sort_order', false, 'down', '{up,down}' ),
+			new ELH_ApiRequestParameter( 'min_price', false, false, 'float' ),
+			new ELH_ApiRequestParameter( 'max_price', false, false, 'float' ),
+			new ELH_ApiRequestParameter( 'color', false, false, 'color' ),
+			new ELH_ApiRequestParameter( 'color_accuracy', false, 0, '[0-30]' ),
+			new ELH_ApiRequestParameter( 'tags', false, false, '[string]' ),
+			new ELH_ApiRequestParameter( 'category', false, false, 'string' ),
+			new ELH_ApiRequestParameter( 'translate_keywords', false, 0, 'bool' ),
+			new ELH_ApiRequestParameter( 'include_private', false, 0, 'bool' ),
+		);
+	}
 
-    /**
-     * @return string
-     */
-    public function method()
-    {
-        return 'findAllShopListingsActive';
-    }
+	/**
+	 * @return string
+	 */
+	public function uri() {
+		return '/users/:shop_id/listings/active';
+	}
 
-    /**
-     * @return bool
-     */
-    public function requires_OAuth()
-    {
-        return false;
-    }
+	/**
+	 * @return string
+	 */
+	public function method() {
+		return 'findAllShopListingsActive';
+	}
 
-    /**
-     * @return string
-     */
-    public function permission_scope()
-    {
-        return 'none';
-    }
+	/**
+	 * @return bool
+	 */
+	public function requires_OAuth() {
+		return false;
+	}
 
-    public function http_method()
-    {
-        return 'GET';
-    }
+	/**
+	 * @return string
+	 */
+	public function permission_scope() {
+		return 'none';
+	}
 
-    public function parameters()
-    {
-        return $this->parameters;
-    }
+	public function http_method() {
+		return 'GET';
+	}
+
+	public function parameters() {
+		return $this->parameters;
+	}
 }
