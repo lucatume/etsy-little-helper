2744cc437612f1bfceb50c69e961a4c577da4106
Luca Tumedei <luca@theaveragedev.com>
2015-04-26 21:11:24 +0200

    removed comment

--- src/ELH/TypeUtils.php
+++ src/ELH/TypeUtils.php
@@ -3,17 +3,6 @@
 
 class ELH_TypeUtils {
 
-//	new ELH_ApiRequestParameter('sort_on', false, 'created', '{created,price,score}'),
-//	new ELH_ApiRequestParameter('sort_order', false, 'down', '{up,down}'),
-//	new ELH_ApiRequestParameter('min_price', false, false, 'float'),
-//	new ELH_ApiRequestParameter('max_price', false, false, 'float'),
-//	new ELH_ApiRequestParameter('color', false, false, 'color'),
-//	new ELH_ApiRequestParameter('color_accuracy', false, 0, 'int[0-30]'),
-//	new ELH_ApiRequestParameter('tags', false, false, '[string]'),
-//	new ELH_ApiRequestParameter('category', false, false, 'string'),
-//	new ELH_ApiRequestParameter('translate_keywords', false, 0, 'bool'),
-//	new ELH_ApiRequestParameter('include_private', false, 0, 'bool'),
-
 	public static function is_a( $value, $type ) {
 		if ( ! is_scalar( $value ) ) {
 			return false;
